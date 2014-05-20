6816c41f3532daaff1e879caa48b404fe8aa5078
Matt Krantz <mlkrantz@cs.dartmouth.edu>
2014-05-20 12:46:51 -0400

    Fixed AMStartup boundary case, modified Makefile

--- Makefile
+++ Makefile
@@ -2,13 +2,18 @@ CC = gcc
 CFLAGS =  -Wall -pedantic -std=c11 -g -D_GNU_SOURCE
 
 AMStartup: AMStartup.o
-	@$(CC) $(CFLAGS) -o AMStartup AMStartup.o
+	@$(CC) $(CFLAGS) -o AMStartup src/AMStartup.o
 	@echo "Building AMStartup"
 
 AMStartup.o: src/AMStartup.c src/amazing.h
-	@$(CC) $(CFLAGS) -c src/AMStartup.c
-	@echo "Building /src/AMStartup.o"
+	@$(CC) $(CFLAGS) -c src/AMStartup.c -o src/AMStartup.o
+	@echo "Building src/AMStartup.o"
 
 clean:
+	@rm -f *~
+	@rm -f *#
 	@rm -f *.o
 	@rm -f AMStartup
+	@rm -f ./src/*~
+	@rm -f ./src/*#
+	@rm -f ./src/*.o
--- src/AMStartup.c
+++ src/AMStartup.c
@@ -4,7 +4,7 @@
  * Author:
  * Date: 05/19/14
  *
- * Input: None.
+ * Input: None
  * 	
  *
  * Command line options:
@@ -23,7 +23,7 @@
  */
 /* ========================================================================== */
 // ---------------- Open Issues
-// None.
+// None
 
 // ---------------- System includes e.g., <stdio.h>
 #include <stdio.h>                           
@@ -31,7 +31,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <strings.h>  // bcopy()
+#include <strings.h>  				// bcopy()
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -43,7 +43,7 @@
 #include "amazing.h"
 
 // ---------------- Constant definitions
-static const char USAGE[] = "Usage: ./AMStartup -n nAvatars -d Difficulty -h Hostname [--help]\n";
+static const char USAGE[] = "Usage: ./AMStartup -n nAvatars -d Difficulty -h Hostname [--help]";
 
 // ---------------- Macro definitions
 #define VERBOSE 0 		// for debugging
@@ -68,49 +68,49 @@ int main(int argc, char *argv[]) {
 	int numAvatars;		// 1-AM_MAX_AVATAR
 	int difficulty;		// 0-AM_MAX_DIFFICULTY
 	char *hostname;
-	char *givenNumAvatars;
-	char *givenDifficulty;
+	char *givenNumAvatars = NULL;
+	char *givenDifficulty = NULL;
 	
 	// for command line option processing
 	int ch;	
-	extern char *optarg;	// parameter of an option
+	extern char *optarg;		// parameter of an option
 	extern int optind;		// current index of argument array
 
 
-    // for client-server communication
-	int sockfd;							// socket file descriptor
+    	// for client-server communication
+	int sockfd;					// socket file descriptor
 	struct sockaddr_in serverAddr;
 	struct hostent *server;
 	
 	
 	// process options
-	while (( ch = getopt_long(argc, argv, "n:d:h:x", longOpts, 0)) != -1) {
+	while ((ch = getopt_long(argc, argv, "n:d:h:x", longOpts, 0)) != -1) {
 		switch(ch){
 			case 'n':
-				givenNumAvatars = optarg;			
-				break;
+			givenNumAvatars = optarg;			
+			break;
 
 			case 'd':
-				givenDifficulty = optarg;
-				break;
+			givenDifficulty = optarg;
+			break;
 			
 			case 'h':
-				hostname = optarg;
-				break;
+			hostname = optarg;
+			break;
+			
 			case 'x':
-				help = 1;
-				break;
+			help = 1;
+			break;
 
 			// unknown options
 			default:
-				fprintf(stderr, "%s\n", USAGE);
-				exit(EXIT_FAILURE);
-				break;
+			fprintf(stderr, "%s\n", USAGE);
+			exit(EXIT_FAILURE);
+			break;
 		}
 	}
 
 	if (help) {
-		userHelp();
 		userHelp();
 		exit(EXIT_SUCCESS);
 	}
@@ -142,14 +142,14 @@ int main(int argc, char *argv[]) {
 	bcopy((char *) server->h_addr, (char *) &serverAddr.sin_addr.s_addr, server->h_length);     
 	serverAddr.sin_port = htons(atoi(AM_SERVER_PORT));
 
-    // Connect to the server
+    	// Connect to the server
 	if (connect(sockfd, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0){
 		fprintf(stderr, "Error: Failed to connect to server.\n");
 		exit(EXIT_FAILURE);
 	}
 	printf("Connection established!\n");
 
-    // create message send
+    	// create message send
 	AM_Message initMsg;
 	initMsg.type = htonl(AM_INIT);
 	initMsg.init.nAvatars = htonl(numAvatars);
@@ -187,39 +187,44 @@ int main(int argc, char *argv[]) {
 	    printf("AM_INIT_OK message successfully received\n");
 	}
 
-    // create the log file
-    int userNameLen = strlen(getenv("USER")) ;
-    char *userName = calloc(userNameLen + 1, sizeof(char));
-    userName = getenv("USER");
-    char *logFileName = NULL;
-    logFileName = (char *)malloc((strlen("Amazing___.log") + strlen(userName))*sizeof(char) + sizeof(int)*2 + 1);
-    sprintf(logFileName, "Amazing_%s_%d_%d.log", userName, numAvatars, difficulty);
+	// create the log file
+	int userNameLen = strlen(getenv("USER")) ;
+	char *userName = calloc(userNameLen + 1, sizeof(char));
+	userName = getenv("USER");
+	char *logFileName = NULL;
+	logFileName = (char *)malloc((strlen("Amazing___.log") + strlen(userName))*sizeof(char) + sizeof(int)*2 + 1);
+	sprintf(logFileName, "Amazing_%s_%d_%d.log", userName, numAvatars, difficulty);
     
-    FILE *logFile;
-    if ((logFile = fopen(logFileName, "a"))== NULL){
-        fprintf(stderr, "Error: Couldn't create log file, %s.\n", logFileName);
-        free(logFileName);
-        exit(EXIT_FAILURE);
-    }
+	FILE *logFile;
+	if ((logFile = fopen(logFileName, "a"))== NULL){
+		fprintf(stderr, "Error: Couldn't create log file, %s.\n", logFileName);
+		free(logFileName);
+		exit(EXIT_FAILURE);
+	}
     
-    time_t rawCurrTime;
-    struct tm *timeinfo;
+	time_t rawCurrTime;
+	struct tm *timeinfo;
 
-    time (&rawCurrTime);
-    timeinfo = localtime(&rawCurrTime);
-    fprintf(logFile, "%s, %d, %s", userName, ntohl(serverMessage.init_ok.MazePort), asctime(timeinfo));
-    fclose(logFile);
-    printf("Log file created as: %s\n", logFileName);
-    free(logFileName);
+	time (&rawCurrTime);
+	timeinfo = localtime(&rawCurrTime);
+	fprintf(logFile, "%s, %d, %s", userName, ntohl(serverMessage.init_ok.MazePort), asctime(timeinfo));
+	fclose(logFile);
+	printf("Log file created as: %s\n", logFileName);
+	free(logFileName);
 	return 0;
 }
 
 
 /*
  * Check that arguments are valid. If successful return 1, else 0.
+ *
  */
 int checkArgs(int argc, char givenDifficulty[], char givenNumAvatars[]) {
 	if (argc != 7) {
+		fprintf(stderr, "%s\n", USAGE);
+		return 0;
+	}
+	if (!givenDifficulty || !givenNumAvatars){
 		fprintf(stderr, "%s\n", USAGE);
 		return 0;
 	}
@@ -251,6 +256,7 @@ int checkArgs(int argc, char givenDifficulty[], char givenNumAvatars[]) {
 
 /*
  * Checks that a user input is numerical. Returns 1 if numerical, else 0.
+ *
  */
 int isNumerical(char inputToCheck[]) {
     for (int i = 0; i < strlen(inputToCheck); i++) {
@@ -264,6 +270,7 @@ int isNumerical(char inputToCheck[]) {
 
 /*
  * Prints help information, including version and usage.
+ *
  */
 void userHelp() {
 	printf("The Amazing Project\n");
@@ -271,4 +278,3 @@ void userHelp() {
 	printf("Version 1.0\n");
 	printf("%s\n", USAGE);
 }
-
